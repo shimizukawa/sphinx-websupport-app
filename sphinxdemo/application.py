76a9da369e92711f19c3c5e4f5efad67279792a5
jacob <jacob@panda>
2010-05-31 17:13:27 -0500

    Update to work with current version of web support package.

--- sphinxdemo/application.py
+++ sphinxdemo/application.py
@@ -5,20 +5,13 @@ from sphinxdemo import conf
 
 app = Flask(__name__)
 
-# Template that will be used to render comments on the page.
-comment_html = """
-<a href="#" onclick="alert('[ comment stub &lt;{{ id }}&gt; ]');">comment</a>
-"""
 support.init(srcdir=conf.DOCTREE_ROOT,
-             outdir=conf.OUTPUT_DIR,
-             comment_html=comment_html)
-
+             outdir=conf.OUTPUT_DIR)
 
 @app.route('/build')
 def build():
     support.build()
     return 'Build Complete'
-
 
 @app.route('/docs/')
 @app.route('/docs/<docname>')
