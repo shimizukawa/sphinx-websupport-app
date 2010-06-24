9a4c84d5717a882138e53121e6992186df2a7af4
jacob <jacob@panda>
2010-06-24 14:18:24 -0500

    added xapian search

--- sphinxdemo/application.py
+++ sphinxdemo/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from sphinx.websupport import support
 
 from sphinxdemo import conf
@@ -6,7 +6,8 @@ from sphinxdemo import conf
 app = Flask(__name__)
 
 support.init(srcdir=conf.DOCTREE_ROOT,
-             outdir=conf.OUTPUT_DIR)
+             outdir=conf.OUTPUT_DIR,
+             search='xapian')
 
 @app.route('/build')
 def build():
@@ -17,6 +18,11 @@ def build():
 @app.route('/docs/<path:docname>')
 def doc(docname='contents'):
     document = support.get_document(docname)
+    return render_template('doc.html', document=document)
+
+@app.route('/docs/search')
+def search():
+    document = support.get_search_results(request.args.get('q', ''))
     return render_template('doc.html', document=document)
 
 if __name__ == '__main__':
