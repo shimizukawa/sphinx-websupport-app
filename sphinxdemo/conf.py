15b1881c5895db5456da24c1cc74688c74627036
Jacob Mason <JacobAMason@users.noreply.github.com>
2010-08-12 13:15:20 -0500

    added easy click links for google and yahoo oid

--- sphinxdemo/conf.py
+++ sphinxdemo/conf.py
@@ -1,8 +1,8 @@
 
 DEBUG = True
 
-SOURCE_DIR = '/home/jacob/sphinx/doc'
-BUILD_DIR = '/home/jacob/Desktop/testbuild'
-DATABASE_URI = 'sqlite:////home/jacob/Desktop/sphinx-demo.db'
-SECRET_KEY = 'eij3489afjOJ83jf9hf'
-SEARCH = 'xapian'
+SOURCE_DIR = ''
+BUILD_DIR = ''
+DATABASE_URI = ''
+SECRET_KEY = ''
+SEARCH = ''
