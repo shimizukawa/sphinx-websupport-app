511a79a7c0b0358d64806f0b1c2ea46ff7b3503a
Jacob Mason <JacobAMason@users.noreply.github.com>
2010-08-16 12:27:40 -0500

    copy static files from build dir to demo static dir after build

--- build.py
+++ build.py
@@ -8,7 +8,8 @@
     :copyright: Copyright 2007-2010 by the Sphinx team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
-
+import os
+import shutil
 
 from flask import Flask
 
@@ -27,3 +28,15 @@ support = WebSupport(srcdir=app.config['SOURCE_DIR'],
                      search=app.config['SEARCH'],
                      storage=app.config['DATABASE_URI'])
 support.build()
+
+demo_static_dir = os.path.join(os.getcwd(), 'sphinxdemo', 'static')
+
+static_dirs = ['_static', '_sources', '_images']
+
+for directory in static_dirs:
+    src = os.path.join(app.config['BUILD_DIR'], 'static', directory)
+    dst = os.path.join(demo_static_dir, directory)
+    if os.path.isdir(src):
+        if os.path.isdir(dst):
+            shutil.rmtree(dst)
+        shutil.copytree(src, dst)
