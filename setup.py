ce1016bdda9da4a637c350ac9a805fe4cfcb923c
Takayuki SHIMIZUKAWA <shimizukawa at gmail dot com>
2015-02-11 15:28:02 +0900

    install package data files

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author="Jacob Mason",
     author_email="noreply@example.org",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Flask',
         'Flask-OpenID',
